8642af6d9cb6a35793f9598c28e607d1743fe9b9
lee-shun <lee970802@163.com>
2022-06-30 19:49:58 -0400

    u

---
+++ run_rgbd_densemap.bash
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# 1
+# ./bin/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM1.yaml ./TUM_dataset/rgbd_dataset_freiburg1_desk2 Examples/RGB-D/associations/fr1_desk2.txt
+
+# 2
+# ./bin/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM1.yaml ./TUM_dataset/rgbd_dataset_freiburg2_xyz Examples/RGB-D/associations/fr2_xyz.txt
+
+# 3
+./bin/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM1.yaml ./TUM_dataset/rgbd_dataset_freiburg3_long_office_household ./Examples/RGB-D/associations/fr3_office.txt
